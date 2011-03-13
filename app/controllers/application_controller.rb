9d6969e5543614d277ec28d97c72f5687df2129f
Owen Evans <owen@iouk.com>
2011-03-13 20:48:26 +1300

    adding webdevcon.org redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_filter :check_uri
 
   def check_uri
-    if /[.]*webdevcon.org/.match(request.host)
-      redirect_to request.protocol + "webdev.org.nz"+request.request_uri
+    if /[.]*webdev.org.nz/.match(request.host)
+      redirect_to request.protocol + "webdevcon.org"+request.request_uri
     elsif /^www/.match(request.host)
       redirect_to request.protocol + request.host_with_port[4..-1] + request.request_uri 
     end
