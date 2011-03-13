705f0b3a0d9498695e8ef60e37d9764e5410b4d5
Owen Evans <owen@iouk.com>
2011-03-13 20:05:47 +1300

    inital project

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  
+  def index
+    @title = "WDCNZ - Home"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebdevconOrg::Application.config.secret_token = '1cad24258f9e9efea23265215b4338485c49003a6c3955586b1efffd7792c1e69fa31d28c365fdff1fce0b8915a9329b78629f4dae66f25923170a8ce66d4e75'
