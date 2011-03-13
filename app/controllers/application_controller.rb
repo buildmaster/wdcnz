42ca5991b105add8e87e0298cc6e611c1c33040e
Owen Evans <owen@iouk.com>
2011-03-13 20:26:30 +1300

    add redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :check_uri
+
+  def check_uri
+    redirect_to request.protocol + request.host_with_port + request.request_uri if /^www/.match(request.host)
+  end
 end
