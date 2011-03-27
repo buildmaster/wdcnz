ede4f008e0f8982e4dfa2c3794103910248bb852
Owen Evans <owen@iouk.com>
2011-03-28 09:31:07 +1300

    slide transitions

--- public/javascripts/email-form.js
+++ public/javascripts/email-form.js
@@ -23,5 +23,34 @@ jQuery(function($){
    
    $("#email").Watermark("ADD YOUR EMAIL FOR UPDATES","#D8D8D9");
    WDCNZ.init();
+ 	$("#slide1 .right").bind('click',function(){
+		$('#slide1').removeClass('selected');
+		$('#slide1').addClass('previous');
+		$('#slide2').addClass('selected');
+		$('#slide2').removeClass('next')
+		return false;
+	});
+
+	$("#slide2 .left").bind('click',function(){
+		$('#slide2').removeClass('selected');
+		$('#slide2').addClass('next');
+		$('#slide1').addClass('selected');
+		$('#slide1').removeClass('previous')
+		return false;
+	});
+   $("#slide2 .right").bind('click',function(){
+		$('#slide2').removeClass('selected');
+		$('#slide2').addClass('previous');
+		$('#slide3').addClass('selected');
+		$('#slide3').removeClass('next')
+		return false;
+	});
+   $("#slide3 .left").bind('click',function(){
+		$('#slide2').addClass('selected');
+		$('#slide2').removeClass('previous');
+		$('#slide3').removeClass('selected');
+		$('#slide3').addClass('next')
+		return false;
+	});
 });
 
