2571f41c7d6981ff905661a470f077d232d46111
Owen Evans <owen@bgeek.net>
2014-03-07 19:57:37 +1300

    remove rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Wdcnz2012::Application.config.secret_token = '33e8b03bf5cfa8845822a0af7955fdccb89cf325a6fcf4d005b0a62dcee164e112a175509758d6e5b2fbd491ff0cb3332d062f0c962d85696ef7c8818cbe8255'
